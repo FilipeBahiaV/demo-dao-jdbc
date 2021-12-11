8e143819289437f9f97bbfc411c2a5a3b471bb76
Filipe Bahia <filipebahiaviannarodrigues@gmail.com>
2021-12-10 21:30:14 -0300

    insert and findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -10,17 +11,29 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+		System.out.println("==== Seller findById =====");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		
+		
 		System.out.println(" ===== Seller findByDepartment =======");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		System.out.println(" ===== Seller findAll =======");
+	    list = sellerDao.findAll();
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n ==== Seller insert ======");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Insert! New id = " + newSeller.getId());
+		
 	}
 
 }
