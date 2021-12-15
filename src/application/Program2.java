3727458ab5fdcee835c29da0600fa68cd3781edd
Filipe Bahia <filipebahiaviannarodrigues@gmail.com>
2021-12-15 09:50:03 -0300

    DepartmentDao DeleteById final implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,6 @@
 package application;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -9,20 +10,20 @@ public class Program2 {
 	public static void main(String[] args) {
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-		
+		Scanner sc = new Scanner(System.in);
 		
 		Department depart = departmentDao.findById(3);
 		System.out.println(depart);
 		
 		
 		
-		Department dep = new Department(null, "Food");
+		Department dep = new Department(null, "Comics");
 		departmentDao.insert(dep);
 		System.out.println("Insert! New id = " + dep.getId());
 		
 		
-		Department depa = departmentDao.findById(9);
-		depa.setName("health");
+		Department depa = departmentDao.findById(6);
+		depa.setName("D3");
 		departmentDao.update(depa);
 		System.out.println("Update executed: ");
 		
@@ -31,6 +32,12 @@ public class Program2 {
 		for(Department bb : list) {
 			System.out.println(bb);
 		}
+		
+		
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Deleted");
 
 	}
 
